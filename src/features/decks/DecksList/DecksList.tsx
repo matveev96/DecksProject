58398c06334903e110bee75d8087d9dc7e978964
matveev96 <sasha.matveev1996@icloud.com>
2025-02-05 08:46:45 +0300

    refactor: refactor of naming

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -2,13 +2,12 @@ import s from './DecksList.module.css'
 import { useEffect } from 'react'
 import { decksApi } from '../decks-api.ts'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
-import { type DecksState, setDeckAC } from '../decks-reducer.ts'
-import { type RootState, useAppDispatch, useAppSelector } from '../../../app/store.ts'
+import { setDeckAC } from '../decks-reducer.ts'
+import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectDecks } from '../decks-selectors.ts'
 
 export const DecksList = () => {
-
-  const desks = useAppSelector(selectDecks)
+  const decks = useAppSelector(selectDecks)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -20,7 +19,7 @@ export const DecksList = () => {
 
   return (
     <ul className={s.list}>
-      {desks.decks.map((deck) => (
+      {decks.decks.map((deck) => (
         <DeckItem deck={deck} key={deck.id} />
       ))}
     </ul>
