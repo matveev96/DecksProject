308bbb57f4eb649557d82a858c0d37174ae179d2
matveev96 <sasha.matveev1996@icloud.com>
2025-02-05 08:35:49 +0300

    feat: add deckReducer to project

---
+++ src/features/decks/decks-reducer.ts
@@ -0,0 +1,31 @@
+import type { Items } from '../../common/types/types.ts'
+
+const initialState = {
+  decks: [] as Items[], // todo: add type
+  searchParams: {
+    name: '',
+  },
+}
+
+export type DecksState = typeof initialState
+
+export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
+  switch (action.type) {
+    case 'SET_DECK': {
+      return {...state, decks: action.payload }
+    }
+    default:
+      return state
+  }
+}
+
+export const setDeckAC = (payload: Items[]) => {
+  return {
+    type: 'SET_DECK',
+    payload
+  } as const
+}
+
+type setDeckAT = ReturnType<typeof setDeckAC>
+
+type DecksActions = setDeckAT
