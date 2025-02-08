4988ee3b02b7a1ac574cf682784ea1df7bf80ed7
matveev96 <sasha.matveev1996@icloud.com>
2025-02-08 11:08:04 +0300

    feat: add types AppThunk and AppDispatch in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,20 @@
-import { combineReducers, legacy_createStore, applyMiddleware, AnyAction } from 'redux'
-import thunkMiddleware, { ThunkDispatch } from 'redux-thunk'
+import thunkMiddleware, { type ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { decksReducer } from '../features/decks/decks-reducer.ts'
+import { applyMiddleware, combineReducers, legacy_createStore, type UnknownAction } from 'redux'
 
 const rootReducer = combineReducers({
   decksReducer,
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware))
+export const store = legacy_createStore(rootReducer, {},applyMiddleware(thunkMiddleware))
 
 export type AppRootState = ReturnType<typeof rootReducer>
 
-export type AppDispatch = ThunkDispatch<AppRootState, unknown, AnyAction>
-
 export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+export type AppThunk = ThunkAction<void, RootState, unknown, UnknownAction>
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
