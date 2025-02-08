b6e24242927974fd9ad9d11f04f86af82d1f9b1e
matveev96 <sasha.matveev1996@icloud.com>
2025-02-08 11:12:15 +0300

    feat: realize dispatch in form (onSubmit)

--- src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
+++ src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
@@ -1,11 +1,16 @@
 import s from './AddNewPostForm.module.css'
 import { useForm } from 'react-hook-form'
+import { useAppDispatch } from '../../../app/store.ts'
+import { addDecksTC } from '../decks-thunks.ts'
 
-type FormValues = {
+export type FormValues = {
   name: string
 }
 
 export const AddNewDeckForm = () => {
+
+  const dispatch = useAppDispatch()
+
   const {
     register,
     handleSubmit,
@@ -17,7 +22,7 @@ export const AddNewDeckForm = () => {
   })
 
   const onSubmit = (data: FormValues) => {
-    console.log(data)
+    dispatch(addDecksTC(data.name))
   }
 
   return (
