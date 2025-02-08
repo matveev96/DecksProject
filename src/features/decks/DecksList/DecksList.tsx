e93fca57f5ccb8f426f8fbac584de8fd98e48806
matveev96 <sasha.matveev1996@icloud.com>
2025-02-08 11:10:07 +0300

    refactor: use ThunkCreator (fetchDecksTC) in dispatch

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -3,19 +3,21 @@ import { useEffect } from 'react'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
 import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectDecks } from '../decks-selectors.ts'
-import { fetchDecksThunk } from '../decks-thunks.ts'
+import { fetchDecksTC } from '../decks-thunks.ts'
 
 export const DecksList = () => {
   const decks = useAppSelector(selectDecks)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(fetchDecksThunk)
+    dispatch(fetchDecksTC())
   }, [])
 
   return (
     <ul className={s.list}>
-      {decks.decks.map((deck) => (<DeckItem deck={deck} key={deck.id} />))}
+      {decks.decks.map((deck) => (
+        <DeckItem deck={deck} key={deck.id} />
+      ))}
     </ul>
   )
 }
