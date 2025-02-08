4263a3e1f74848b9a6e4fc7028bc63149c0ab3b2
matveev96 <sasha.matveev1996@icloud.com>
2025-02-08 11:15:59 +0300

    feat: add ThunkCreator which create Deck

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,12 +1,17 @@
 import { decksApi } from './decks-api.ts'
-import { setDeckAC } from './decks-reducer.ts'
-import { Dispatch } from 'redux'
-import type { RootState } from '../../app/store.ts'
+import { addDeckAC, setDeckAC } from './decks-reducer.ts'
+import type { AppThunk } from '../../app/store.ts'
 
-export const fetchDecksThunk = (dispatch: Dispatch, getState: () => RootState) => {
+export const fetchDecksTC = (): AppThunk => (dispatch) => {
   decksApi.fetchDecks().then((res) => {
     const deckItems = res.data.items
     dispatch(setDeckAC(deckItems))
   })
 }
 
+export const addDecksTC = (name: string): AppThunk  => (dispatch) => {
+    decksApi.addDecks(name).then((res) => {
+      const item= res.data
+      dispatch(addDeckAC(item))
+    })
+}
